0822762bf8df30705d8823bba6406a6ca7c807b2
chenyiteng <1842710211@qq.com>
2025-02-25 15:56:05 +0800

    dealt with collision

--- photo.py
+++ photo.py
@@ -76,7 +76,6 @@ if __name__ == '__main__':
     data_path = "/home/ur5/rekep/ReKepUR5_from_kinova/data"
 
     prs = PRS()
-    time.sleep(2)
     try:
         color_image, depth_image = prs.get_frames()
 
